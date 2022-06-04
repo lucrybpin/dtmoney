310ff2a82d8fead3e19149b21a5d49ab874f30ef
Lucas R Pinto <lucspinto@outlook.com>
2022-06-04 18:34:14 -0300

    End of chapter two

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+import Modal from "react-modal";
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { GlobalStyle } from "./styles/global";
+
+Modal.setAppElement('#root');
+
 export function App() {
+
+  const [isNewTransationModalOpen, setIsNewTransationModalOpen] = useState(false);
+
+  function handleOpenNewTransationModal() {
+    setIsNewTransationModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransationModalOpen(false);
+  }
+
   return (
-    <div className="App">
-      <h1>Life is Painful but Beautiful</h1>
-    </div>
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransationModal} />
+      <Dashboard />
+
+      <NewTransactionModal 
+        isOpen={isNewTransationModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
+      <GlobalStyle />
+    </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
+
+createServer({
+
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          amount: 6000,
+          createdAt: new Date('2022-06-01 21:12:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1400,
+          createdAt: new Date('2022-06-02 12:00:00')
+        }
+      ]
+    });
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    })
+
+    this.post('transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data);
+    });
+
+  }
+});
 
 
 const root = ReactDOM.createRoot(
